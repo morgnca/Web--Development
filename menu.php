a7343e0c17b88dba256c6945e491d2a3cfa37d18
morgnca <110359481+morgnca@users.noreply.github.com>
2022-10-03 18:08:18 +1300

    Header image added

--- menu.php
+++ menu.php
@@ -76,6 +76,8 @@ if ($sort == 'vege') {
 	
 		<body>
 			<header>
+				<img id="header_image" src="images/wegc_logo.png" alt="The logo for Wellington East Girls' College">
+				<br>
 				<h1>Wellington East Girl's College</h1>
 				<h1>Cafe Website</h1>
 			</header>
