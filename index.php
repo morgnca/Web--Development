8388be3757f72a3827ecf56f21f8f9b82f496e39
morgnca <110359481+morgnca@users.noreply.github.com>
2022-10-03 17:39:52 +1300

    base page for website
    empty but will add text later

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+		<title>Menu Homepage</title>
+		<meta name="description" content="This site gives allows the user to view the menu for Wellington East Girls College."/>
+		<meta name="robots" content= "noindex, nofollow" />
+		<meta name="author" content="M C-Arkell 2022" />
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="assessment.css">
+		<!--<link rel="icon" href="images/favicon.ico" type="image/x-icon"/>-->
+</head>
+
+<body>
+	<header>
+		<h1>Wellington East Girl's College</h1>
+		<br>
+		<h1>Cafe Website</h1>
+	</header>
+	
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="specials.php">Specials</a></li>
+		</ul>
+	</nav>
+	
+	<main>
+		<p>description
+	</main>
+</body>
+
+<footer>
+	<p> Morgan C-Arkell 2022
+</footer>
+	
+</html>
